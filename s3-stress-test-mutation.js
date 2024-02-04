f142076bea32c3ba7b81118aaf3dc1050038d9f5
Andrey Kozlov <a.kozlov@kt-team.de>
2024-02-04 18:17:42 +0400

    S3[126387]- tests scripts

--- s3-stress-test-mutation.js
+++ s3-stress-test-mutation.js
@@ -1,16 +1,15 @@
 import http from 'k6/http';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-import { Trend, Rate, Counter, Gauge } from 'k6/metrics';
-
+import {Rate} from 'k6/metrics'
 
 const apiKey = 'b267882a2ac6eefe6918d99e128fb9cb';
 const apiURL = 'http://pimcore-preprod.terminal.lft/pimcore-graphql-webservices/stressTest';
 
-export const _fail_response = new Rate('fail_response');
+export const failResponse = new Rate('fail_response');
 
 export const options = {
-    // vus: 3,
+    summaryTrendStats: ["min","avg","med","max","p(90)","p(95)","p(99)"],
     thresholds: {
         http_req_failed: [
             {
@@ -23,28 +22,28 @@ export const options = {
                 threshold: 'rate <= 0.1',
                 abortOnFail: true,
             }
-        ],
+        ]
     },
     stages: [
-        {duration: '5m', target: 5},
-        {duration: '5m', target: 10},
-        {duration: '5m', target: 15},
-        {duration: '5m', target: 20},
-        {duration: '5m', target: 25},
-        {duration: '5m', target: 30},
-        {duration: '5m', target: 35},
-        {duration: '5m', target: 40},
-        {duration: '5m', target: 45},
-        {duration: '5m', target: 50},
-        {duration: '5m', target: 55},
-        {duration: '5m', target: 60},
-        {duration: '5m', target: 65},
-        {duration: '5m', target: 70},
-        {duration: '5m', target: 75},
-        {duration: '5m', target: 80},
-        {duration: '5m', target: 85},
-        {duration: '5m', target: 90},
-        {duration: '5m', target: 95},
+        // {duration: '5m', target: 5},
+        // {duration: '5m', target: 10},
+        // {duration: '5m', target: 15},
+        // {duration: '5m', target: 20},
+        // {duration: '5m', target: 25},
+        // {duration: '5m', target: 30},
+        // {duration: '5m', target: 35},
+        // {duration: '5m', target: 40},
+        // {duration: '5m', target: 45},
+        // {duration: '5m', target: 50},
+        // {duration: '5m', target: 55},
+        // {duration: '5m', target: 60},
+        // {duration: '5m', target: 65},
+        // {duration: '5m', target: 70},
+        // {duration: '5m', target: 75},
+        // {duration: '5m', target: 80},
+        // {duration: '5m', target: 85},
+        // {duration: '5m', target: 90},
+        // {duration: '5m', target: 95},
         {duration: '5m', target: 100},
         {duration: '5m', target: 105},
         {duration: '5m', target: 110},
@@ -80,11 +79,8 @@ export const options = {
         {duration: '5m', target: 265},
         {duration: '5m', target: 270},
         {duration: '5m', target: 275},
-    ],
-    // duration: '1m',
-    // iterations: 10
-    summaryTrendStats: ["min","avg","med","max","p(90)","p(95)","p(99)"],
-};
+    ]
+}
 
 function generateRandomString(length) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -176,38 +172,31 @@ export default function () {
         JSON.stringify({ query }),
         { headers }
     );
-    try {
+    if (response.status === 200) {
         const json= response.json();
-        const success = json.data.createProduct.success
+        const success = !!json.data.createProduct.success
         if (success === false) {
-            const fail_response = true;
-            // console.log(response.json())
-            _fail_response.add(fail_response);
+            console.log(response.status)
+            failResponse.add(true);
         } else {
-            const fail_response = false;
-            // console.log('OK')
-            _fail_response.add(fail_response);
+            failResponse.add(false);
         }
-    } catch (error) {
-        // JSON не корректен
-        // console.error('JSON не корректен');
-        const fail_response = true;
-        // console.log(response)
-        _fail_response.add(fail_response);
+    } else {
+        console.log(response.status)
+        failResponse.add(true);
     }
 }
 export function handleSummary(data) {
     const now = new Date();
     const formattedDate = `${now.getDate()}-${now.getMonth() + 1}-${now.getFullYear()}`;
     const formattedTime = `${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}`;
-    const fileName = `./json/summary_${formattedDate}-${formattedTime}.json`;
-    const fileName2 = `./html/result_${formattedDate}-${formattedTime}.html`;
+    const json = `./json/summary_${formattedDate}-${formattedTime}.json`;
+    const html = `./html/result_${formattedDate}-${formattedTime}.html`;
     return {
         'stdout': textSummary(data, { indent: ' ', enableColors: true }),
-        [fileName]: JSON.stringify(data),
-        [fileName2]: htmlReport(data),
-
-    };
+        [json]: JSON.stringify(data),
+        [html]: htmlReport(data)
+    }
 }
 //Установка k6
 // sudo snap install k6
